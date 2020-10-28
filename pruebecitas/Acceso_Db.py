c8fc8625a5e503e32119cf03a4db93babf961696
Oscarini <osjaumi@gmail.com>
2020-10-28 11:19:27 +0100

    version 2

--- pruebecitas/Acceso_Db.py
+++ pruebecitas/Acceso_Db.py
@@ -3,7 +3,7 @@ from mysql.connector import Error
 
 db = mysql.connector.connect(host="localhost", user="oscar",passwd="oscar", db="mydb")
 cursor = db.cursor()
-recs=cursor.execute("SELECT * FROM usuarios")
+recs=cursor.execute("SELECT * FROM pedidos p, usuarios u where p.idusuarios=u.idusuarios")
 
 
 
